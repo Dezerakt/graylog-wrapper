86d0def0359cdc33c6817058fcb2bbd8ecd9b23c
AIsaev <isaev3658@gmail.com>
2023-09-15 12:25:39 +0600

    delete return err for comfort, and some struct changes

--- main.go
+++ main.go
@@ -3,33 +3,46 @@ package graylog_wrapper
 import (
 	"bytes"
 	"encoding/json"
+	"github.com/fatih/color"
+	"log"
 	"net/http"
 )
 
 type GraylogConfig struct {
 	address string
+	message string
+	host    string
+	body    interface{}
 }
 
 func Init(address string) *GraylogConfig {
-	return &GraylogConfig{address: address}
+	return &GraylogConfig{
+		address: address,
+		message: "empty",
+		host:    "localhost",
+	}
 }
 
-func (g *GraylogConfig) WriteLog(message interface{}) error {
-	marshalledMessage, err := json.Marshal(message)
+func (g *GraylogConfig) WriteLog(body interface{}, shortMessage string) {
+	g.body = body
+	if shortMessage != "" {
+		g.message = shortMessage
+	}
+
+	marshalledMessage, err := json.Marshal(g)
 	if err != nil {
-		return err
+		log.Println(color.RedString(err.Error()))
 	}
 
 	request, err := http.NewRequest(http.MethodPost, g.address, bytes.NewBuffer(marshalledMessage))
 	if err != nil {
-		return err
+		log.Println(color.RedString(err.Error()))
 	}
 
 	client := &http.Client{}
 	_, err = client.Do(request)
 	if err != nil {
-		return err
+		log.Println(color.RedString(err.Error()))
 	}
 
-	return nil
 }
