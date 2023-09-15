f58bb98e48ff10d058b0fe1ea851733209f659fe
AIsaev <isaev3658@gmail.com>
2023-09-15 11:31:57 +0600

    some struct

--- main.go
+++ main.go
@@ -1,7 +1,35 @@
 package graylog_wrapper
 
-import "log"
+import (
+	"bytes"
+	"encoding/json"
+	"net/http"
+)
 
-func WriteLog(message interface{}) {
-	log.Println("something")
+type GraylogConfig struct {
+	address string
+}
+
+func Init(address string) *GraylogConfig {
+	return &GraylogConfig{address: address}
+}
+
+func (g *GraylogConfig) WriteLog(message interface{}) error {
+	marshalledMessage, err := json.Marshal(message)
+	if err != nil {
+		return err
+	}
+
+	request, err := http.NewRequest(http.MethodPost, g.address, bytes.NewBuffer(marshalledMessage))
+	if err != nil {
+		return err
+	}
+
+	client := &http.Client{}
+	_, err = client.Do(request)
+	if err != nil {
+		return err
+	}
+
+	return nil
 }
